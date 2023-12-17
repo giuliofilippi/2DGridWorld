34ee77e57c60698b8fcf3272d189bf6cc0e81194
giuliofilippi <giuliofilippi@me.com>
2023-12-17 11:18:29 +0100

    test

--- tests/test_display.py
+++ tests/test_display.py
@@ -15,7 +15,8 @@ from display import render
 world = World(width=100, length=100)
 agents = [Agent(world) for _ in range(50)]
 world.pheromones[[50,20,2], [50,20,2]] = 100  # Set an example pheromone concentration
-world.diffuse_pheromones(0.5, 100) # diffuse
+world.diffuse_pheromones(0.5, 10) # diffuse
+world.evaporate_pheromones(0.1, 10) # evaporate
 
 # Visualize the world state
 render(world.grid, world.pheromones, show=True, save=True, name="example_image.png")
